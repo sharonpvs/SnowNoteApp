15dd79befc5ae6471b4b7f744ac080ecd0f4918d
sharonpvs <sharonpvs@hotmail.com>
2017-01-22 06:16:47 -0800

    homescreen updated
    image shows along with title bar color

--- SnowNoteApp/app/src/main/java/com/example/ranko/snownoteapp/MainActivity.java
+++ SnowNoteApp/app/src/main/java/com/example/ranko/snownoteapp/MainActivity.java
@@ -1,20 +1,74 @@
 package com.example.ranko.snownoteapp;
 
-import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //inherit previous onCreate
         super.onCreate(savedInstanceState);
 
-        setTitle("Snow Note");
+        //sets master content view
         setContentView(R.layout.activity_main);
+
+        // setting image resource from drawable
+        Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.snownotetop);
+        Bitmap bMapScaled = Bitmap.createScaledBitmap(bMap, 500, 200, true);
+        ImageView image = (ImageView) findViewById(R.id.startlogo);
+        image.setImageBitmap(bMapScaled);
         //Transition mFadeTransition = new Fade();
         //TransitionManager.go(mEndingScene, mFadeTransition);
     }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+    private Drawable resize(Drawable image) {
+        Bitmap bitmap = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(bitmap,
+                (int) (bitmap.getWidth() * 0.5), (int) (bitmap.getHeight() * 0.5), false);
+        return new BitmapDrawable(getResources(), bitmapResized);
+    }
+   /* public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }*/
 }
