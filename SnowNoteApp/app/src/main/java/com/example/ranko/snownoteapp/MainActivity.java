ee3fb7bb6b56897ad4e38d35f6ac3920ad3f7ba9
sharonpvs <sharonpvs@hotmail.com>
2017-01-21 19:54:54 -0800

    Title Screen updated
    Title screen is green with proper name

--- SnowNoteApp/app/src/main/java/com/example/ranko/snownoteapp/MainActivity.java
+++ SnowNoteApp/app/src/main/java/com/example/ranko/snownoteapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.ranko.snownoteapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setTitle("Snow Note");
         setContentView(R.layout.activity_main);
+        //Transition mFadeTransition = new Fade();
+        //TransitionManager.go(mEndingScene, mFadeTransition);
     }
 }
