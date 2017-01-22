c00294d2a670e81d211c526d3cd3d6c8ef6b3a2c
sharonpvs <sharonpvs@hotmail.com>
2017-01-21 17:03:48 -0800

    main screen displays on emulator
    added two layouts to be home screen animation. work in progress.

---
+++ SnowNoteApp/app/src/main/java/com/example/ranko/snownoteapp/OpenScene.java
@@ -0,0 +1,13 @@
+package com.example.ranko.snownoteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenScene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_scene);
+    }
+}
